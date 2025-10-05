00bd63ad43430d695244fcb513003688a955d618
Daniel Valencia <danyvalencia0505@hotmail.com>
2025-10-05 11:46:25 -0700

    Primer commit: configuración inicial de React y Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">Daniel Valencia</h1>
+      <p>Welcome to my portfolio!</p>
+    </div>
+  )
+}
+
+export default App
